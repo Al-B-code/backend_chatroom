4b92664eb761b424a7a4c335dddb6c178642d4c9
Emily Reid <emily.reid31@gmail.com>
2023-11-13 17:27:11 +0000

    refactored and completed annotations for UserController

--- backend_chatroom/src/main/java/com/group_1/backend_chatroom/services/UserService.java
+++ backend_chatroom/src/main/java/com/group_1/backend_chatroom/services/UserService.java
@@ -5,14 +5,18 @@ import com.group_1.backend_chatroom.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Optional;
 
-public class userService {
+public class UserService {
 
-   @Autowired
+    @Autowired
     UserRepository userRepository;
 
-   public List<User> users(){
-       return userRepository.findAll();
-   }
+    public List<User> users(){
+        return userRepository.findAll();
+    }
 
+    public Optional<User> getUser(Long id) {
+        return userRepository.findById(id);
+    }
 }
