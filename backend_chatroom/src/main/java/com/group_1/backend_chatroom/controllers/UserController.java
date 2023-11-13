09079a5c3e7335d454cb6863d0f9eb25e6c3f10a
Albert Hughes <albieh@me.com>
2023-11-13 17:58:34 +0000

    changed @RequestParam in userController getUserById to @PathVariable

--- backend_chatroom/src/main/java/com/group_1/backend_chatroom/controllers/UserController.java
+++ backend_chatroom/src/main/java/com/group_1/backend_chatroom/controllers/UserController.java
@@ -5,10 +5,7 @@ import com.group_1.backend_chatroom.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,7 +23,7 @@ public class UserController {
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> getUserById(@RequestParam Long id){
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
         User user = userService.getUser(id).get();
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
